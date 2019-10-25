882b4b46473bad920d435b0ad79528ad5e69a525
Sebastian Malcher <sebastianlukasmalcher@gmail.com>
2019-10-25 15:08:21 +0200

    Adds more css

--- js/script.js
+++ js/script.js
@@ -1,28 +1,26 @@
 initPage();
 
-function initPage(){
+function initPage() {
     createUserProfiles()
 }
 
 
-function createUserProfiles(){
+function createUserProfiles() {
 
-    for(i=0;i<user_data.length;i++)
-        {
-
-
-            if(i%4==0){
-                let row = $("<div></div>").addClass("row m-0 p-0");
-                $("content").append(row);
-            }
-
-            
-            let profile = $(`<div></div>`).addClass("col p-0 mx-2 my-5 bg-dark border-top border-dark rounded-top").html(`<img style="width:90%; height:100%;"
-            class="mx-auto d-block bg-dark" src='${user_data[i].image}'><div class="border border-dark rounded-bottom" id="userDesc">${user_data[i].quote}</div>`).css("height","150px");
-            $("content>.row:last-child").append(profile);
-            // if(i%4 == 0){
-            //     let lgScreenBreak = $("<div></div>").addClass('w-100 d-none d-lg-block');
-            //     $(`content>div:nth-child(${i})`).after(lgScreenBreak);
-            // }
+    for (i = 0; i < user_data.length; i++) {
+        if (i % 4 == 0) {
+            let row = $("<div></div>").addClass("row mb-5 mt-3 p-0");
+            $("content").append(row);
         }
+
+        let profile = $(`<div></div>`).addClass("col p-0 mx-3 mb-4 rounded-top").html(`<span class="fas fa-heart"></span><img style="width:90%; height:100%;"
+            class="mx-auto d-block bg-dark" src='${user_data[i].image}'><div class="rounded-bottom" id="userDesc">${user_data[i].quote}</div>`).css("height", "150px");
+        $("content>.row:last-child").append(profile);
+        // if(i%4 == 0){
+        //     let lgScreenBreak = $("<div></div>").addClass('w-100 d-none d-lg-block');
+        //     $(`content>div:nth-child(${i})`).after(lgScreenBreak);
+        // }
+    }
 }
+
+//pdorner github
